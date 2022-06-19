36c874a2524f70e71d9aee2f243a9e8f241a0617
Tâm BE <tam.cl@tradingfoe.com>
2022-06-19 15:04:05 +0700

    refactor code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,8 @@
 require 'application_responder'
 
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
+
   self.responder = ApplicationResponder
 
   layout 'application'
