d40806426b07c894e702cfc49027d0275bf0c55d
Tâm BE <tam.cl@tradingfoe.com>
2022-06-19 14:42:29 +0700

    add csrf token

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,4 +35,9 @@ class ApplicationController < ActionController::Base
   def current_user_id
     current_user&.id
   end
+
+  def verify_authenticity_token
+    request.headers['X-CSRF-Token'] ||= request.headers['X-XSRF-TOKEN']
+    super
+  end
 end
