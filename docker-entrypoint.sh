c5d819b08ba7eeb78c018b15515f7107a595cb6d
Tâm BE <tam.cl@tradingfoe.com>
2022-06-19 14:23:34 +0700

    config docker entrypoint

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
 
-#echo "Migrate DB"
-#bundle exec rake db:migrate
-#
-#echo "Seed DB."
-#bundle exec rake db:seed
-#
-# echo "Start sidekiq"
-# bundle exec sidekiq -C config/sidekiq.yml -c $SIDEKIQ_CONCURRENCY -d
+echo "Migrate DB"
+bundle exec rake db:migrate
+
+echo "Seed DB."
+bundle exec rake db:seed
 
 echo "Start Puma server"
 bundle exec puma -C config/puma.rb
